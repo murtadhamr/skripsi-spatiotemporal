bf42e4fbbaaf976d0c05ebbe334df627100d407c
Muhammad Murtadha Ramadhan <muhmurtadha29@gmail.com>
2018-03-10 05:40:13 +0700

    attributes value fixed

--- skripsi-svr.R
+++ skripsi-svr.R
@@ -6,15 +6,26 @@ x = dataset[, 5:8]
 
 #selecting c02 value from dataframe x
 co2 = x[, 3]
+lonlat = x[, 1:2]
 
 #finding 9 nearest point from will-be-predicted point
-nearest = as.data.frame(knn.index(co2, k=9, algorithm = "kd_tree"))
+nearest = knn.index(lonlat, k=9, algorithm = "kd_tree")
 
 #making blank dataframe to store concentration value from nearest point(by index)
 dataset2 = as.data.frame(matrix(nrow = 25789, ncol = 9))
 
 #storing nearest concentration value to dataset2
 for (i in 1:9){
-  dataset2[, i] = co2[nearest[, i]]
+  for (j in 1: 25789){
+    dataset2[j, i] = co2[nearest[j, i]]
+  }
 }
+
+#merging attributes with target
+dataco2 = merge(dataset2, co2)
+
+#using SVR
+library(e1071)
+
+
 
